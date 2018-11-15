c2bd4449d0d74cc4bf3d36cf11c41db2f4b6b0e1
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2018-11-15 14:15:42 -0200

    finis config webpack

--- exercicios_webpack/webpack.config.js
+++ exercicios_webpack/webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
     entry: './ex/index.js',
@@ -10,15 +11,21 @@ module.exports = {
         port: 8080,
         contentBase: './public'
     },
+    plugins: [
+        new ExtractTextPlugin('app.css')
+    ],
     module: {
         loaders: [{
             test: /.js?$/,
             loader: 'babel-loader',
             exclude: /node_modules/,
             query: {
-                presets: ['es2015'],
+                presets: ['es2015', 'react'],
                 plugins: ['transform-object-rest-spread']
             }
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
         }]
     }
 }
