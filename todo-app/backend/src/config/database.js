fb0f3f99c1ed2ff3d47674b1797e4424ece9c008
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2018-11-22 16:59:04 -0200

    Put backend in docker

--- todo-app/backend/src/config/database.js
+++ todo-app/backend/src/config/database.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-module.exports = mongoose.connect('mongodb://localhost:27017/todo')
+module.exports = mongoose.connect('mongodb://db/todo')
