1b103c3a86a35d63515e2763b6b5229d9329becc
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2018-12-05 23:19:26 -0200

    config redux

--- exercicios_react/ex/index.jsx
+++ exercicios_react/ex/index.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { combineReducers, createStore } from 'redux'
+import { Provider } from 'react-redux'
+
 import Field from './field'
 
+const reducers = combineReducers({
+    field: () => ({ value: 'Opa' })
+})
+
 ReactDOM.render(
-    <Field initialValue='Teste' />
+    <Provider store={createStore(reducers)}>
+        <Field initialValue='Teste' />
+    </Provider>
 , document.getElementById('app'))
